90a4817d31629fe685fd80d55081b24f39507106
Hejociara <hejociarav@gmail.com>
2022-10-10 17:34:58 -0300

    Initial commit

---
+++ primeirosPassosSpring/src/test/java/Dio/springBoot/PrimeirosPassosSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package Dio.springBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeirosPassosSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
